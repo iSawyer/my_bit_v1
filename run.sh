289599ac73ba45e2d9a5366cb81123445ab12b92
Sawyer <174471146@qq.com>
2016-02-14 17:08:18 +0800

    11
    1

--- run.sh
+++ run.sh
@@ -1,10 +1,4 @@
-#########################################################################
-# File Name: run.sh
-# Author: dongsui
-# mail: suidong@baidu.com
-# Created Time: 日  2/ 7 00:12:17 2016
-#########################################################################
 #!/bin/bash
 scrapy crawl bit-spider -a crawl_type=crawl_url 
-scrapy crawl bit-spider -a crawl_type=crawl_thread thres=$1
+scrapy crawl bit-spider -a crawl_type=crawl_thread
 
