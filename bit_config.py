8e1585d1dc5d9cff29517d021cd7f09a85bb7de5
Sawyer <174471146@qq.com>
2016-02-14 17:22:10 +0800

    1
    2

---
+++ bit_config.py
@@ -0,0 +1,9 @@
+# -*- coding:utf-8 -*-
+
+#论坛置顶帖等无用帖子
+forbid_url = ['thread-10442489-','thread-10480774-','thread-10287865-','thread-10534784-','thread-334630-','thread-10471436-','thread-10556444-','thread-10474940-','thread-10556345-','thread-10548923-','thread-10510780-','thread-10599468-','thread-10498674-','thread-24238-','thread-10526403-','thread-10574471-','thread-10104915-','thread-10076285-','thread-10550120-','thread-466827-','thread-10542992-','thread-10542991-','thread-361469-','thread-4373-','thread-25103-','hread-125421-','thread-125421-','thread-217728-','thread-320656-']
+
+#灌水 前程似锦 硬件与s数码 网络技术 软件使用 游戏 体坛 职场 | 爬去的页数，一页有16个帖子
+forum_list = [('forum-14',350),('forum-83',175),('forum-80',11),('forum-10',13),('forum-21',4),('forum-22',4),('forum-115',9),('forum-124',17)]
+
+home_url = 'http://out.bitunion.org'
--- my_bit_v1/spiders/bit_spider.py
+++ my_bit_v1/spiders/bit_spider.py
@@ -8,22 +8,30 @@ from urllib import urlencode
 from scrapy.selector import Selector
 from scrapy.contrib.linkextractors.sgml import SgmlLinkExtractor
 from my_bit_v1.items import MyBitV1Item
-import sys
 import datetime,calendar
 from scrapy import optional_features
 from bs4 import BeautifulSoup
+import sys
+#sys.path.append('')
+#import os
+#print os.getcwd()
+from bit_config import *
 optional_features.remove('boto')
 
+
+
+#while(1):
+#	pass
 
 insert_time = str(datetime.date.today())
 
 
-forbid_url = ['thread-10442489-','thread-10480774-','thread-10287865-','thread-10534784-','thread-334630-','thread-10471436-','thread-10556444-','thread-10474940-','thread-10556345-','thread-10548923-','thread-10510780-','thread-10599468-','thread-10498674-','thread-24238-','thread-10526403-','thread-10574471-','thread-10104915-','thread-10076285-','thread-10550120-','thread-466827-','thread-10542992-','thread-10542991-','thread-361469-','thread-4373-','thread-25103-','hread-125421-','thread-125421-','thread-217728-','thread-320656-']
+#forbid_url = ['thread-10442489-','thread-10480774-','thread-10287865-','thread-10534784-','thread-334630-','thread-10471436-','thread-10556444-','thread-10474940-','thread-10556345-','thread-10548923-','thread-10510780-','thread-10599468-','thread-10498674-','thread-24238-','thread-10526403-','thread-10574471-','thread-10104915-','thread-10076285-','thread-10550120-','thread-466827-','thread-10542992-','thread-10542991-','thread-361469-','thread-4373-','thread-25103-','hread-125421-','thread-125421-','thread-217728-','thread-320656-']
 file_name = 'url_file'
 
 #灌水 前程似锦 硬件与s数码 网络技术 软件使用 游戏 体坛  职场
-forum_list = [('forum-14',350),('forum-83',175),('forum-80',11),('forum-10',13),('forum-21',4),('forum-22',4),('forum-115',9),('forum-124',17)]
-home_url = 'http://out.bitunion.org'
+#forum_list = [('forum-14',350),('forum-83',175),('forum-80',11),('forum-10',13),('forum-21',4),('forum-22',4),('forum-115',9),('forum-124',17)]
+#home_url = 'http://out.bitunion.org'
 
 
 
